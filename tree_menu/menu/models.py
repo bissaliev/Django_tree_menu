cc5f70c11696797230f582309cacff5f70e4f473
bissaliev <bissaliev21@gmail.com>
2024-04-11 10:00:16 +0400

    Создание модели меню

--- tree_menu/menu/models.py
+++ tree_menu/menu/models.py
@@ -2,4 +2,12 @@ from django.db import models
 
 
 class Menu(models.Model):
-    pass
+    """Model Menu."""
+    name = models.CharField(max_length=150, verbose_name="Название меню")
+    parent = models.ForeignKey(
+        to="self",
+        related_name="children",
+        on_delete=models.CASCADE,
+        null=True, blank=True,
+        verbose_name="Родительское меню"
+        )
